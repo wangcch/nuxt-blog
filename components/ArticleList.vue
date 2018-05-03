77e1a036b8b821338da0b7f2e698c41f445f38c3
wangcch <ci713@qq.com>
2018-05-03 16:50:51 +0800

    update index page blog_list

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="article-list">
+    <article-excerpt v-for="(item,index) in data" :key="item.title + index" :data="item" class="list-article"></article-excerpt>
   </div>
 </template>
 
@@ -7,6 +8,13 @@
 import ArticleExcerpt from '~/components/ArticleExcerpt.vue'
 export default {
   name: "artcle-list",
+  props: ['data'],
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
   components: {
     ArticleExcerpt
   }
@@ -14,4 +22,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.article-list {
+  .list-article {
+    margin-top: 20px;
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <section class="container">
     <top-show :data="topShow" :topTag="true"></top-show>
-    <el-row :gutter="10">
-      <el-col :xs="24" :sm="8">
-        <article-list :data="articleDataList"></article-list>
+    <el-row>
+      <el-col :xs="24" :sm="14" :md="16">
+        <div class="blog-list">
+          <h1 class="blog-list_title">Recent News and Blogs</h1>
+          <article-list :data="articleDataList"></article-list>
+          <div class="blog-list_more">
+            <router-link to="/blog">See All</router-link>
+          </div>
+        </div>
       </el-col>
-      <el-col :xs="24" :sm="16">
+      <el-col :xs="24" :sm="10" :md="8">
+        <div class="index-more">
+          more
+        </div>
       </el-col>
     </el-row>
   </section>
@@ -62,11 +71,36 @@ export default {
 
 <style lang="scss" scoped>
 .container {
+  overflow: hidden;
   .top-show {
     margin: 30px 0;
   }
-  .article-list {
-    background: #aaa;
+  .blog-list, .index-more {
+    padding: 30px;
+    @media (max-width: 768px) {
+      padding: 20px;
+    }
+  }
+  .blog-list {
+    border: 1px solid #E4E7ED;
+    background: #ffffff;
+    border-radius: 3px;
+    .blog-list_title {
+      margin: 0 0 20px 0;
+      font-size: 22px;
+      color: #303133;
+    }
+  }
+  .blog-list_more {
+    text-align: right;
+    margin-top: 30px;
+    a {
+      text-decoration: none;
+      color: #3b5998;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 }
 </style>
